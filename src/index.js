ddea7bed670af5c72f24a096a7ea04a24c17e60f
RomuloPales <romulopales15@gmail.com>
2023-02-18 11:22:03 -0300

    final version

--- src/index.js
+++ src/index.js
@@ -30,11 +30,34 @@ app.post("/tweets" ,(req,res) => {
             res.status(401).send({ message: "UNAUTHORIZED" });
             return;
     }
+    if(!tweet){
+        res.status(400).send({ error: "Todos os campos são obrigatórios" });
+        return;
+    }
     tweets.push({username, tweet})
     res.status(201).send({ message: "OK" });
 })
 
+app.get("/tweets", (req, res) => {
+    const lastTen = tweets.slice(-10)
 
+    tweets.forEach((tweet) => {
+        const { avatar } = users.find((user) => user.username === tweet.username);
+        tweet.avatar = avatar;
+      });
+      
+    res.send(lastTen)
+})
+
+app.get("/tweets/:username", (req, res) => {
+    const {username} = req.params
+
+    const tweetsUser = tweets.filter(
+        (tweet) => tweet.username.toLowerCase() === username.toLowerCase()
+      );
+    
+      res.send(tweetsUser);
+    });
 
 app.listen(5000, () => {
   console.log("server running in port 5000");
